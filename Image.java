c0463a4b2160296d3a7fdfc74de49a6494e43e2e
Imene Baraitame <baraitame.imene@gmail.com>
2021-07-01 20:16:13 +0100

    read an image

---
+++ Image.java
@@ -0,0 +1,17 @@
+package ocr;
+
+import java.awt.image.BufferedImage;
+
+public class Image{
+
+
+	//loading an image from a file
+	String imagePath1;
+	BufferedImage myPicture;
+			Image (String imagePath1, BufferedImage myPicture){
+				this.imagePath1 = imagePath1;
+				this.myPicture = myPicture;
+			}
+			
+
+}
