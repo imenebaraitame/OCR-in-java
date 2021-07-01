f429f4fa12f8470bcac20a927587ec2b197f4475
Imene Baraitame <baraitame.imene@gmail.com>
2021-07-01 20:17:18 +0100

    convert image to text

---
+++ Imgtext.java
@@ -0,0 +1,25 @@
+package ocr;
+
+import java.io.File;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class Imgtext extends Tesseract {
+	String imagePath2;
+	
+	public Imgtext(String imagePath2) throws Exception {
+		this.imagePath2 = imagePath2;
+		
+	}
+
+	String text() throws TesseractException {
+		
+		this.setTessVariable("user_defined_dpi", "300");
+		this.setDatapath("D:\\Tesseract-OCR\\tessdata");//set the Eng language
+		String fulltext = this.doOCR(new File(imagePath2));
+		return fulltext ;
+		
+	}
+
+}
