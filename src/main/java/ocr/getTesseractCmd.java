dc839fb60fbe59086382e6b2388af190b3583a9c
Imene Baraitame <baraitame.imene@gmail.com>
2021-07-17 18:51:35 +0100

    create searchable text image

---
+++ src/main/java/ocr/getTesseractCmd.java
@@ -0,0 +1,48 @@
+package ocr;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class getTesseractCmd {
+	String input_file; String output_file; String tesseract_install_path ;String configfile;
+	
+	getTesseractCmd(String input_file, String output_file, String tesseract_install_path ,String configfile){
+		this.input_file = input_file;
+		this.output_file = output_file;
+		this.tesseract_install_path = tesseract_install_path;
+		this.configfile = configfile;
+	}
+		
+	public void searchableImg() throws IOException {
+		
+      String[] command ={ "cmd" , };
+		 
+		 try {
+		 // Instantiating Process object
+		 Process p = Runtime.getRuntime().exec(command);
+		 // By using geterrorStream() method is to get the error stream of p object
+		        new Thread( new MyRunnable(p.getErrorStream(), System.err)).start();
+		 // By using getInputStream() method is to get the input stream of p object       
+		        new Thread(new MyRunnable(p.getInputStream(), System.out)).start();
+		        PrintWriter stdin = new PrintWriter(p.getOutputStream());
+		        stdin.println("\""+tesseract_install_path+"\" \""+input_file+"\" \""+output_file+"\" \""+configfile+"\" ");
+		        stdin.close();
+		       
+		        int exitValue = p.waitFor();
+				if (exitValue != 0) {
+				    System.out.println("Abnormal process termination");
+				}
+				// Destroy a process
+				p.destroy();
+				if (p.exitValue() != 0) {
+				    System.out.println("Abnormal process termination");
+				}
+		        
+		    } catch (Exception e) {
+		             e.printStackTrace();
+		        }
+
+	}
+	
+
+}
