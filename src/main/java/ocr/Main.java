23cc3eefb1752a2baad338977cf0d6b8df0c3f13
Imene Baraitame <baraitame.imene@gmail.com>
2021-08-03 20:31:48 +0100

    Create searchable pdf with the original image

--- src/main/java/ocr/Main.java
+++ src/main/java/ocr/Main.java
@@ -1,5 +1,15 @@
 package ocr;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
+
+
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
@@ -7,25 +17,72 @@ public class Main {
 		String imagePath = Main.class.getResource("/images/test2.jpg").getPath();
 		ImgProcess image = new ImgProcess(imagePath );
 		String imageDeskew = image.deskewImage(imagePath);
-		String imageMagick = image.magickManipulation(imageDeskew);
-	    	String finalImage = image.bufferedImage(imageMagick);
+		String imageNBorder = image.removeBorder(imageDeskew);
+		String blackNWhite = image.bufferedImage(imageNBorder);
+		String imageMagick = image.magickManipulation(blackNWhite);
+		String finalImage = image.imageTransparent(imageNBorder,imageMagick);
+	    
 
-        	//Extract text from the image.
-		Imgtext ocr = new Imgtext(finalImage);
-		String fulltext = ocr.text();
+        //Extract text from the image.
+		//Imgtext ocr = new Imgtext(finalImage);
+		//String fulltext = ocr.text();
 		
 		
-		System.out.println("Creating pdf document...");
-		TextPdf textpdf = new TextPdf(fulltext, "./ocrDemo.pdf");
-		System.out.println("Document created.");		
-		textpdf.document();
+		//System.out.println("Creating pdf document...");
+		//TextPdf textpdf = new TextPdf(fulltext, "./ocrDemo.pdf");
+		//System.out.println("Document created.");		
+		//textpdf.document();
 		
 		//Place an invisible text layer on the top of the image and create a searchable pdf.
-		TesseractCmd cmd = new TesseractCmd(finalImage, "./textonly_pdf", "tesseract", "pdf");
+		// configfileValue = 0->make the image visible, =1->make the image invisible
+		TesseractCmd cmd = new TesseractCmd(finalImage,"./textonly_pdf","0");
 		cmd.textOnlyPdf(finalImage);
-		//cmd.searchableImg();
 		
-  
+		
+ 
+        System.out.println("getting the size and the location of the image from textonly_pdf");
+		PDDocument document = null;
+		Path path = Paths.get("textOnly_pdf.pdf");
+		String inputFilePath= path.toAbsolutePath().toString();
+        
+        String outputFilePath = "./newFile.pdf"; // New file
+		
+         try {
+        	      
+        	document = PDDocument.load(new File(inputFilePath));
+            GetImageLocationsAndSize printer = new GetImageLocationsAndSize();
+            int pageNum = 0;
+            for( PDPage page : document.getPages() )
+            {
+                pageNum = pageNum + 1;
+                
+                printer.processPage(page);
+            }
+            document.close();
+            System.out.println("Document created.");
+            
+         //Place the original image on top of transparent image in existing PDF.
+            printer.merge(inputFilePath, outputFilePath, imageNBorder);
+         }
+         finally {
+        	 if(document != null)
+        	 {
+        		 document.close();
+        	 }
+         }
+		
+         
+		
+		
+		
+		
+		
+		
+		
+       
+	
+	 
+        
 	}
 
 }
