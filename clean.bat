c1517f6d17ce653a04bad2f8c38bfff8d558f13d
Imene Baraitame <baraitame.imene@gmail.com>
2021-08-18 18:33:36 +0100

    add script

--- clean.bat
+++ clean.bat
@@ -1,3 +1,3 @@
 mvn clean
-rm binaryInverseImg.png borderRemoved.png deskewImage.png transparentImg.png newFile.pdf ocrDemo.pdf textonly_pdf.pdf
-del binaryInverseImg.png borderRemoved.png deskewImage.png transparent.png newFile.pdf ocrDemo.pdf textonly_pdf.pdf
+rm binaryInverseImg.png borderRemoved.png deskewImage.png transparentImg.png ExtractedImage_1.png newFile.pdf ocrDemo.pdf textonly_pdf.pdf
+del binaryInverseImg.png borderRemoved.png deskewImage.png transparent.png ExtractedImage_1.png newFile.pdf ocrDemo.pdf textonly_pdf.pdf
