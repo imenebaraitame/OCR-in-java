97c006a76141c524926b1dffbb4d37a7d2518e28
imenebaraitame <baraitame.imene@gmail.com>
2021-08-21 15:41:22 +0100

    rename method

--- src/main/java/ocr/FileExtension.java
+++ src/main/java/ocr/FileExtension.java
@@ -5,13 +5,13 @@ import org.apache.commons.io.FilenameUtils;
 public class FileExtension {
 
 	
-	public static String getExtension(String filename) {
+	public static String getExtensionOfFile(String filename) {
 	    return FilenameUtils.getExtension(filename);
 	}
 	public static boolean isPng(String filename) {
-		return FileExtension.getExtension(filename).contains("png");	
+		return FileExtension.getExtensionOfFile(filename).contains("png");	
 	}
 	public static boolean isPdf(String filename) {
-		return FileExtension.getExtension(filename).contains("pdf");	
+		return FileExtension.getExtensionOfFile(filename).contains("pdf");	
 	}
 }
