559c109d37894838c4f4bc302c0bfada9b98533a
imenebaraitame <baraitame.imene@gmail.com>
2021-08-28 18:55:50 +0100

    add call to each command

--- clean.bat
+++ clean.bat
@@ -1,3 +1,3 @@
-mvn clean
-rm binaryInverseImg.png borderRemoved.png deskewImage.png transparentImg.png ExtractedImage_1.png newFile.pdf ocrDemo.pdf textonly_pdf.pdf bufferedImage.png magickManipulation.png removeBorder.png
-del binaryInverseImg.png borderRemoved.png deskewImage.png transparent.png ExtractedImage_1.png newFile.pdf ocrDemo.pdf textonly_pdf.pdf bufferedImage.png magickManipulation.png removeBorder.png
+call mvn clean
+call rm borderRemoved.png binaryInverseImg.png deskewImage.png transparentImg.png ExtractedImage_1.png newFile.pdf textonly_pdf.pdf
+call del binaryInverseImg.png borderRemoved.png deskewImage.png transparentImg.png ExtractedImage_1.png newFile.pdf textonly_pdf.pdf
