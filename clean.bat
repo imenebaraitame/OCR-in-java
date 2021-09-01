ed74661ffa3ba9fe18119243e11d190b0a95fbbe
imenebaraitame <baraitame.imene@gmail.com>
2021-09-01 15:22:37 +0000

    refactoring

--- clean.bat
+++ clean.bat
@@ -1,3 +1,3 @@
-call mvn clean
-call rm borderRemoved.png binaryInverseImg.png deskewImage.png transparentImg.png ExtractedImage_1.png newFile.pdf textonly_pdf.pdf
+mvn clean
+rm borderRemoved.png binaryInverseImg.png deskewImage.png transparentImg.png ExtractedImage_1.png newFile.pdf textonly_pdf.pdf
 call del binaryInverseImg.png borderRemoved.png deskewImage.png transparentImg.png ExtractedImage_1.png newFile.pdf textonly_pdf.pdf
